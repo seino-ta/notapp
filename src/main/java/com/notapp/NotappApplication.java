e1b6cc0d76f0d10c07be03eedbaf4f625df46738
seino_ta <seino.service@gmail.com>
2020-11-05 23:51:27 +0900

    first commit

---
+++ src/main/java/com/notapp/NotappApplication.java
@@ -0,0 +1,13 @@
+package com.notapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notapp/NotappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
